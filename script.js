1de5d7582b12c0d6dea3a7824a6e2805a71da435
zwiecktel <a.zichittella@gmail.com>
2021-11-27 18:11:59 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+import CryptoJS  from  '/node_modules/crypto-js/crypto-js.js'
+
+// Encrypt
+var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
+
+// Decrypt
+var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+console.log(originalText); // 'my message'
